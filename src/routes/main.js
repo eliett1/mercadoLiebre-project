a41d3405c8934253c6e5b0e765565e10eb99865c
eliett1 <eliett1@gmail.com>
2021-01-03 19:06:20 -0300

    new comment

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-// ************ Controller Require ************
+// ************ Require del control ************
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.index); 
